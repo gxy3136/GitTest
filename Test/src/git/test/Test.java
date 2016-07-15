99007d7d2eef47cf32395ffd2b0950edb7a7b33c
guoxiangyu <guoxiangyu@niwodai.net>
2016-07-15 17:37:00 +0800

    a

---
+++ Test/src/git/test/Test.java
@@ -0,0 +1,22 @@
+package git.test;
+import org.junit.Assert;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		Assert.assertTrue(true);
+		System.out.println("test 001");
+	}
+
+	@org.junit.Test
+	public void test002() {
+		Assert.assertTrue(true);
+	}
+
+	@org.junit.Test
+	public void test003() {
+		System.out.println("test003");
+	}
+
+}
