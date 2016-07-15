c69dbf43276c9b385a2b8964f4c114681b74d15a
guoxiangyu <guoxiangyu@niwodai.net>
2016-07-15 17:26:15 +0800

    update 001 method

--- Test/src/git/test/Test.java
+++ Test/src/git/test/Test.java
@@ -7,11 +7,16 @@ public class Test {
 	@org.junit.Test
 	public void test() {
 		Assert.assertTrue(true);
+		System.out.println("test 001");
 	}
 
 	@org.junit.Test
 	public void test002() {
 		Assert.assertTrue(true);
+	}
+
+	@org.junit.Test
+	public void test003() {
 	}
 
 }
