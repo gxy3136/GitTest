22ca971956f16c2defe9a1bbf3286728c8c13038
guoxiangyu <guoxiangyu@niwodai.net>
2016-07-15 17:20:23 +0800

    update code

--- Test/src/git/test/Test.java
+++ Test/src/git/test/Test.java
@@ -1,10 +1,16 @@
 package git.test;
+
 import org.junit.Assert;
 
 public class Test {
 
 	@org.junit.Test
 	public void test() {
+		Assert.assertTrue(true);
+	}
+
+	@org.junit.Test
+	public void test002() {
 		Assert.assertTrue(true);
 	}
 
